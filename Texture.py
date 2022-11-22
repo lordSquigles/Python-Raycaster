1aef5ca7b874afe28702cd3ac84a4c90ee821f78
tchern0b0g <69250930+lordSquigles@users.noreply.github.com>
2022-11-22 07:32:27 -0600

    Add files via upload

--- Texture.py
+++ Texture.py
@@ -1,18 +1,7 @@
 import pygame
-import numpy as np
 
 class Texture():
-    pixMap = []
-    h = 0
-    w = 0
-    array = np.empty(1, dtype=int)
-    list = []
-    def __init__(self, image):
-        pxArray = pygame.PixelArray(image)
-        self.array = np.ndarray(shape=(image.get_height() * image.get_width()))
-        for y in range(image.get_height()):
-            for x in range(image.get_width()):
-                self.array[x + y * image.get_width()] = int(pxArray[x, y]) # much faster to make a pixel array than to deal with pygame's "Surface" crap
-                self.list.append(pxArray[x, y])
-        self.h = int(image.get_height())
-        self.w = int(image.get_width())
+    def __init__(self, file):
+        image = pygame.image.load(file)
+        array = pygame.PixelArray(image)
+
