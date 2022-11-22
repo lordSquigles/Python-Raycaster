cca2ccaccf23c20445c16634998b1ab2c0658454
tchern0b0g <>
2022-11-22 09:25:41 -0600

    more texture mapping

--- game1.2/render.py
+++ game1.2/render.py
@@ -38,6 +38,7 @@ def render(player, map, window, wallTex, screenW, screenH, pxMult): # my impleme
 
         hit = False # for breaking the loop
         dist = 0 # distance to calc from
+        texNum = 0
         while hit == False: # march our ray based on previous calculations
             if sideDistX < sideDistY: # we increment based on the smallest sideDist so far
                 sideDistX += deltaDistX # step along our ray (float precision walk)
@@ -51,11 +52,12 @@ def render(player, map, window, wallTex, screenW, screenH, pxMult): # my impleme
                 side = 1
 
             if mapX >= 0 and mapX < map.w and mapY >= 0 and mapY < map.h: # make sure that we are within our map
-                if map.array[mapX + mapY * map.w] > 0: # did we hit a wall?
+                texNum = int(map.array[mapX + mapY * map.w]) # get the number in the square to determine which texture we are using
+                if texNum > 0: # did we hit a wall?
+                    texNum -= 1 # we want to start at 0 in our texture array, not 1 whole texture in
                     hit = True # Yes!
             else: break
         if hit: # we hit a wall!
-            texNum = map.array[mapX + mapY * map.w]
             dist -= deltaDistX if side == 0 else deltaDistY # subtract our extra step
 
             intersectionX = player.x + rayDirX * dist # calculate our intersection point (based on javid9x's implementation of DDA algorithm)
@@ -74,14 +76,14 @@ def render(player, map, window, wallTex, screenW, screenH, pxMult): # my impleme
             if pxHeight < 1:
                 for j in range(height):
                     #window.set_at((x, int(player.horizon - height / 2) + j), int(wallTex.array[int(textureX) + int(j * (wallTex.h / height)) * wallTex.w]))
-                    pygame.draw.rect(window, int(wallTex.array[int(textureX) * texNum + int(j * (wallTex.h / height)) * wallTex.w * texNum]), [x * pxMult, (int(player.horizon - height / 2) + j) * pxMult, pxMult, pxMult]) # draw my slice
+                    pygame.draw.rect(window, int(wallTex.array[int(textureX) + texNum * wallTex.h + int(j * (wallTex.h / height)) * wallTex.w]), [x * pxMult, (int(player.horizon - height / 2) + j) * pxMult, pxMult, pxMult]) # draw my slice
                 #for j in range(int(height / 2)):
                 #    pygame.draw.rect(window, int(wallTex.array[int(textureX) + int(j * (wallTex.h / height) * 2) * wallTex.w]), [x, int(player.horizon - height / 2) + j * 2, 2, 2]) # draw my slice
             else:
                 for j in range(wallTex.h): # This bit, I do so that I do not waste my time sampling the same px again and again (like in ssloy's tinyraycaster)
                 #for j in range(0, height, int(pxHeight)):
                     # adding 1 here concedes some redundancy but fixes a more jarring graphical bug
-                    pygame.draw.rect(window, int(wallTex.array[int(textureX) * texNum + j * wallTex.w * texNum]), [x * pxMult, (int(player.horizon - height / 2) + j * pxHeight) * pxMult, pxMult, pxMult * pxHeight + 1])
+                    pygame.draw.rect(window, int(wallTex.array[int(textureX) + texNum * wallTex.h + j * wallTex.w]), [x * pxMult, (int(player.horizon - height / 2) + j * pxHeight) * pxMult, pxMult, pxMult * pxHeight + 1])
 
         pygame.draw.line(window, (255, 255, 255, 20), [screenW - 8, screenH], [screenW + 8, screenH], 2)# draw crosshair
         pygame.draw.line(window, (255, 255, 255, 20), [screenW, screenH - 8], [screenW, screenH + 8], 2)# draw crosshair
